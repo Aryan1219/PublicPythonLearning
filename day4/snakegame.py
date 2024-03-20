8da8512f1aa2cae15ea244a47f4d79514b03edc1
Aryan1219 <aryaaggarwal301@gmail.com>
2024-03-20 20:21:19 +0530

    added code to make the turtles stay 20 px from each other in the centre

--- day4/snakegame.py
+++ day4/snakegame.py
@@ -1,7 +1,18 @@
 from turtle import Turtle, Screen
-import random
 
 
 screen = Screen()
 screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+turtles = []
+for i in range(3):
+    turtles.append(Turtle(shape="square"))
+    turtles[i].color("yellow")
+    turtles[i].penup()
 
+turtles[1].goto(-20, 0)
+turtles[2].goto(-40, 0)
+
+
+screen.exitonclick()
