8278cbcf718d5e59526dd2eaf0b6e039f7e80d6b
Aryan1219 <aryaaggarwal301@gmail.com>
2024-03-21 16:20:53 +0530

    edited variables

--- day4/snake.py
+++ day4/snake.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 
 
 class Snake:
-    bodyparts = []
+    body_parts = []
 
     def __init__(self):
 
@@ -13,11 +13,11 @@ class Snake:
             new_part.color("yellow")
             new_part.penup()
             new_part.goto(pos)
-            self.bodyparts.append(new_part)
+            self.body_parts.append(new_part)
 
     def move(self):
-        for part in range(len(self.bodyparts) - 1, 0, -1):
-            next_x = self.bodyparts[part - 1].xcor()
-            next_y = self.bodyparts[part - 1].ycor()
-            self.bodyparts[part].goto(next_x, next_y)
-        self.bodyparts[0].forward(20)
+        for part in range(len(self.body_parts) - 1, 0, -1):
+            next_x = self.body_parts[part - 1].xcor()
+            next_y = self.body_parts[part - 1].ycor()
+            self.body_parts[part].goto(next_x, next_y)
+        self.body_parts[0].forward(20)
