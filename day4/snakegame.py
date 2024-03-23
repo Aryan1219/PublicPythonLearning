c22d600ab40607a00dc5f7de9be6d685e0e7779e
Aryan1219 <aryaaggarwal301@gmail.com>
2024-03-24 00:06:19 +0530

    added key listening functionality to move the snake

--- day4/snakegame.py
+++ day4/snakegame.py
@@ -2,7 +2,6 @@ import time
 from turtle import Screen
 from snake import Snake
 
-
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
@@ -10,12 +9,15 @@ screen.title("Snake Game")
 
 screen.tracer(0)
 snake = Snake()
-
+screen.listen()
+screen.onkey(snake.move_up, "Up")
+screen.onkey(snake.move_down, "Down")
+screen.onkey(snake.move_left, "Left")
+screen.onkey(snake.move_right, "Right")
 game_on = True
 while game_on:
     screen.update()
-    time.sleep(0.2)
+    time.sleep(0.1)
     snake.move()
-
 
 screen.exitonclick()
