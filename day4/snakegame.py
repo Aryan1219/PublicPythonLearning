893fbe711dff1e7409525d9cda33465d17749e30
Aryan1219 <aryaaggarwal301@gmail.com>
2024-03-24 22:38:40 +0530

    if wall is hit GAMEOVER and Keeps the track of score as well

--- day4/snakegame.py
+++ day4/snakegame.py
@@ -1,7 +1,9 @@
 import time
 from turtle import Screen
 from snake import Snake
-from food import  Food
+from food import Food
+from scoreboard import Scoreboard
+
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
@@ -10,20 +12,29 @@ screen.title("Snake Game")
 screen.tracer(0)
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(snake.move_up, "Up")
 screen.onkey(snake.move_down, "Down")
 screen.onkey(snake.move_left, "Left")
 screen.onkey(snake.move_right, "Right")
-game_on = True
+game_is_on = True
 
-
-while game_on:
+while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
     if snake.head.distance(food) <= 15:
         food.refresh()
-        snake.elongate()
+        scoreboard.update()
+        snake.extend()
+    if (
+            snake.head.xcor() < -295
+            or snake.head.xcor() > 295
+            or snake.head.ycor() < -295
+            or snake.head.ycor() > 295
+    ):
+        game_is_on = False
+        scoreboard.game_over()
 
 screen.exitonclick()
