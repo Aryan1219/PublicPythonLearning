d3bf05fb75fc06d2e97ffcd82ba707e8df248e5c
Aryan1219 <aryaaggarwal301@gmail.com>
2024-03-24 20:10:44 +0530

    now creates food and check if collided, if so, then snake is elongated

--- day4/snakegame.py
+++ day4/snakegame.py
@@ -1,7 +1,7 @@
 import time
 from turtle import Screen
 from snake import Snake
-
+from food import  Food
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
@@ -9,15 +9,21 @@ screen.title("Snake Game")
 
 screen.tracer(0)
 snake = Snake()
+food = Food()
 screen.listen()
 screen.onkey(snake.move_up, "Up")
 screen.onkey(snake.move_down, "Down")
 screen.onkey(snake.move_left, "Left")
 screen.onkey(snake.move_right, "Right")
 game_on = True
+
+
 while game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    if snake.head.distance(food) <= 15:
+        food.refresh()
+        snake.elongate()
 
 screen.exitonclick()
